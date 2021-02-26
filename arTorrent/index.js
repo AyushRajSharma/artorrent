e0c239a8f7f8e8b1f916a614a037235a5a2b5a1e
Ayush Raj <ayushrajsharma8051@gmail.com>
2021-02-26 23:17:41 +0530

    initial commit

---
+++ arTorrent/index.js
@@ -0,0 +1,11 @@
+'use strict';
+const fs = require('fs');
+const bencode = require('bencode');
+const tracker = require('./tracker');
+const torrentParser = require('./torrent-parser');
+
+const torrent = torrentParser.open('puppy.torrent');
+
+tracker.getpeers(torrent, peers => {
+    console.log('list of peers: ', peers);
+});
